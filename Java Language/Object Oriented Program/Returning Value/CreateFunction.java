2a20d6eabd06fde87e62602e46b6eeba55bc2ac9
Shoaib <164977722+asoaib121@users.noreply.github.com>
2024-08-14 15:36:50 +0600

    Add files via upload

---
+++ Java Language/Object Oriented Program/Returning Value/CreateFunction.java
@@ -0,0 +1,12 @@
+
+package ReturningValue;
+
+
+public class CreateFunction {
+
+   int square(int value) {
+       return value*value;
+   }
+           
+    
+}
---
+++ Java Language/Object Oriented Program/Returning Value/ReturningValue.java
@@ -0,0 +1,14 @@
+
+package ReturningValue;
+
+
+public class ReturningValue {
+    public static void main(String[] args) {
+        
+        CreateFunction test=new CreateFunction();
+        
+        
+        System.out.println("Returning value of 5= "+test.square(5));
+    }
+    
+}
