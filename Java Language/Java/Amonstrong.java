aa2ce24adb4f5ce83bb24d7af26988fabc8fd8a0
Shoaib <164977722+asoaib121@users.noreply.github.com>
2024-08-14 15:00:16 +0600

    Add files via upload

---
+++ Java Language/Java/Amonstrong.java
@@ -0,0 +1,29 @@
+
+
+package javacode1.amonstrong;
+
+import java.util.Scanner;
+
+
+public class Amonstrong {
+
+    public static void main(String[] args) {
+        Scanner input =new Scanner(System.in);
+        int a,temp,rev=0,r;
+        System.out.print("Enter a number: ");
+        a=input.nextInt();
+        
+        temp=a;
+        while(temp!=0)
+        {
+        r=temp%10;
+        rev=rev+r*r*r;
+        temp=temp/10;
+        
+        }
+        if(rev==a)
+            System.out.println("It is amonstrong number ");
+        else
+            System.out.println("It is not amonstrong number ");
+    }
+}
