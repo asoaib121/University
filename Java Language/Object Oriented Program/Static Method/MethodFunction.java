c7e8df9736c048a4227254b40a9422ef54643dae
Shoaib <164977722+asoaib121@users.noreply.github.com>
2024-08-14 15:50:29 +0600

    Add files via upload

---
+++ Java Language/Object Oriented Program/Static Method/MethodFunction.java
@@ -0,0 +1,15 @@
+
+package StaticMethod;
+
+
+public class MethodFunction {
+
+    void display (){ 
+        System.out.println("I am not a Static Method");
+    }
+    
+    static void  display1 () {
+        System.out.println("I am Static Method");
+    }
+    
+}
---
+++ Java Language/Object Oriented Program/Static Method/StaticMethod.java
@@ -0,0 +1,14 @@
+
+package StaticMethod;
+
+
+public class StaticMethod {
+    public static void main(String[] args) {
+        
+        MethodFunction sb1= new MethodFunction();
+        sb1.display();
+        
+        MethodFunction.display1();
+    }
+    
+}
